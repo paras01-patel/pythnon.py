8aed6ba496f3fa6b0f7db7ee31bf9ce3ccd31afa
paras01-patel <parschandrawanshi649@gmail.com>
2025-11-24 21:06:02 +0530

    recursion

---
+++ recursion.py
@@ -0,0 +1,62 @@
+# wap to print natural number
+
+
+
+
+# def natural_num(n):
+#     sum=0
+#     if n==0:
+#         return 1
+#     n+sum(n-1)
+#     sum+=1
+#     print(n)
+# n=int(input("enter a  number"))
+# natural_num(n)  z
+
+
+
+
+
+#  natural number
+
+
+# def sum_number(n):
+#     if n==1:
+#         return 1
+#     return n+sum_number(n-1)
+# n=int(input("enter a number"))
+# res=sum_number(n)
+# print(res)
+
+
+
+# factorial number
+
+
+# def sum_number(n):
+#     if n==1:
+#         return 1
+#     return n*sum_number(n-1)
+# n=int(input("enter a number"))
+# res=sum_number(n)
+# print(res)
+
+
+
+
+
+# wap sum of natural number
+
+
+
+# def sum_number(n):
+#     if n==1:
+#         return 1
+#     return n+sum_number(n-1)
+# n=int(input("enter a number"))
+# res=sum_number(n)
+# print(res)
+
+
+
+
