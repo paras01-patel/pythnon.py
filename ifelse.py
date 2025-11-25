301d2bd961b36420aadc2135e7c4cc49bf1767a4
paras01-patel <parschandrawanshi649@gmail.com>
2025-11-25 20:15:10 +0530

    if else

---
+++ ifelse.py
@@ -0,0 +1,107 @@
+# num=int(input("enter a number"))
+# for i in range(1,num+1):
+#     if i%2==0:
+    
+#         print(i)
+    
+    
+    
+    
+# num=int (input("enter a number"))
+# if num>0:
+#     print("postive")
+# elif num<0:
+#     print("negetive")
+# else:
+#     print("zero")
+
+
+
+
+# find greater in two number
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# if a>b:
+#     print("a is greater")
+# else:
+#     print("b is greater")
+
+
+# find greater in three number
+
+# a=int (input("enter a number"))
+# b=int(input("enter a number"))
+# c=int(input("enter a number"))
+
+
+# if a>b and a>c:
+#     print ( "a is greater")
+# elif b>a and b>c:
+#     print("b is greater")
+# else:
+#     print(" c is greater")
+
+
+
+# check number of divisble by 5
+
+
+# n=int (input("enter a number"))
+# if n%5==0:
+#     print("divisble by 5")
+# else:
+#     print("not divisble in 5")
+
+
+# leap year
+
+
+    # year=int(input("enter a year"))
+    # if (year%4==0 and year%100!=0) or year%400==0:
+    #     print("leaf year")
+    # else:
+    #     print("not a leaf year")
+    
+    
+    
+    
+# check eligbility for voting
+
+# age=int(input("enter a number"))
+# if age>=18:
+#     print("you a eligible")
+# else:
+#     print("not a eligible")
+
+# ckeck chararter in vowel
+
+# ch=input("enter a string").lower()
+# if ch in 'aeiou':
+#     print("vowels")
+# else:
+#     print("consonant")
+
+
+
+
+# 9. Check if Number is Multiple of 3 and 5
+
+# num=int(input("enter a number"))
+# if num%5==0 and num%3==0:
+#     print("division 3 and 5")
+# else:
+#     print("not a division 3 and 51")
+
+
+
+
+
+# pass or fail
+
+
+# mark=int(input("enter a number"))
+# if mark>=33:
+#     print("pass")
+# else:
+#     print("fail")
